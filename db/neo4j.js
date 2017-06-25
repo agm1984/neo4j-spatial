8cd1dbc42cb96c5405176deb99500944f821858c
Adam Mackintosh <agm1984@gmail.com>
2017-06-25 13:31:00 -0700

    first commit

--- db/neo4j.js
+++ db/neo4j.js
@@ -2,7 +2,7 @@
 var neo4j = require('neo4j-driver').v1;
 
 // Connect database
-const db = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "d1c4b9f0f6d88eddfdc028d97105d8fa6eddf322db5fa628c1c08c5b8c7e8b03"), {
+const db = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "PASSWORDCHANGEME"), {
   encrypted: false
 }).session();
 
